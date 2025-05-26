209975ea144137d52c93a015f3caf5c5c18cfa27
AliMsa22 <ali.mostafa200311@gmail.com>
2025-05-27 00:09:01 +0300

    Updated

--- src/model/chestxray_cnn_v2.py
+++ src/model/chestxray_cnn_v2.py
@@ -6,19 +6,24 @@ class ChestXrayCNNv2(nn.Module):
     def __init__(self, num_classes=7):
         super(ChestXrayCNNv2, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        # First convolutional layer (input: 1 channel, output: 64 channels)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.pool = nn.MaxPool2d(2, 2)
 
+        # Second convolutional layer (input: 64 channels, output: 128 channels)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
 
+        # Third convolutional layer (input: 128 channels, output: 256 channels)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(256)
 
+        # Dropout layer
         self.dropout = nn.Dropout(0.5)
 
-        self.fc1 = nn.Linear(256 * 28 * 28, 1024)
+        # Fully connected layers
+        self.fc1 = nn.Linear(256 * 56 * 56, 1024)
         self.fc2 = nn.Linear(1024, num_classes)
 
     def forward(self, x):
